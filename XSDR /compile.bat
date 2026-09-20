5705ca5c82ab324b05729205796378e94117ea73
NoobLikesThis <bigandypandy@hotmail.com>
2026-09-20 19:15:54 +0100

    Create compile.bat

---
+++ XSDR /compile.bat
@@ -0,0 +1,25 @@
+python -m PyInstaller -F --icon="xsdr.ico" --noconsole --add-data "templates;templates" --add-data "static;static" injector.py
+@echo off
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo completed
+pause
